2d6cc7e3a0253a741a49a7fdd624378877336945
Din Bandhu Adhikari <adbandhu23@gmail.com>
2024-05-14 16:10:04 +0545

    initial commit

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,14 +1,20 @@
+import heroImage from "../assets/image/hero.png";
+import "../Css/Hero.css";
 const Hero = () => {
   return (
     <>
       <div className="hero-wrapper container">
         <div className="hero-text-wrapper">
-          <h2 className="hero-header">When banking meets minimalist</h2>
+          <h1 className="hero-header">
+            When <span className="highlight">banking</span> meets {""}
+            <span className="highlight">minimalist</span>
+          </h1>
           <p className="hero-para">
             A simpler banking experience for a simpler life.
           </p>
           <li className="hero-link">Learn more ↓</li>
         </div>
+        <img className="hero-image" src={heroImage} alt="hero-image" />
       </div>
     </>
   );
